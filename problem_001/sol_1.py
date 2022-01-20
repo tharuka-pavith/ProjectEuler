2a7a65985133ddbcbf542a074f8bf0f608b9eac2
tharuka-pavith <tharukapavith2@gmail.com>
2022-01-20 13:03:16 +0530

    Update docstring of problem_001/sol_1.py

--- problem_001/sol_1.py
+++ problem_001/sol_1.py
@@ -6,7 +6,7 @@ The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 
-Solution: 
+Solution: 234168
 
 References :
 - [Wikipedia link to the topic]
@@ -23,9 +23,12 @@ def solution(limit: int = 1000) -> int:
     You can have a detailed explanation about the solution in the
     module-level docstring.
 
-    ...
-    [Doctest as mentioned above]
-    ...
+    >>> solution(1)
+    0
+    >>> solution(10)
+    23
+    >>> solution(-1)
+    0
 
     """
     ls = [i for i in range(0, limit) if i%3==0 or i%5==0]
