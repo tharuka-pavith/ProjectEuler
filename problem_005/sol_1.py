dc7dca8b12c0025ed958ea82ceb6c9f5ec41d1e0
tharuka-pavith <tharukapavith2@gmail.com>
2022-02-02 16:30:53 +0530

    Update problem_005/sol_1.py

--- problem_005/sol_1.py
+++ problem_005/sol_1.py
@@ -1,5 +1,5 @@
 '''
-Project Euler Problem 006: https://projecteuler.net/problem=7
+Project Euler Problem 006: https://projecteuler.net/problem=5
 
 ---Smallest Multiple---
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
@@ -8,21 +8,22 @@ What is the smallest positive number that is evenly divisible by all of the numb
 Solution: 232792560
 
 References: 
+    - https://en.wikipedia.org/wiki/Least_common_multiple
 
 '''
 
-import math
+def generate_prime_factors(num : int) -> list:
+    '''
+    Generates a list of prime factors of num and returns as a list
+    '''
+    primes = [2,3,5,7,11,13,17,19]
+    pass
 
-def isPrime(num): #2,3,5,7
-    if (num<=1 or num==4): return False
-    if (num==2): return True
 
-    for i in range(2, num ):
-        if num%i == 0: return False
-    return True
-
-nums = [i for i in range(2,21)]
-prime_numbers = [i for i in nums if isPrime(i)]
-#print(prime_numbers)
+def solution() -> int:
+    '''
+    Returns the smallest positive number that is evenly divisible by all of the numbers from 1 to 20
+    '''
+    pass
 
 
