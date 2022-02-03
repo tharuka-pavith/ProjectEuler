14f37dfc1ebe1dca5627282dd17a780047328c62
tharuka-pavith <tharukapavith2@gmail.com>
2022-02-03 10:26:33 +0530

    Create generate_prime_factors()

--- problem_005/sol_1.py
+++ problem_005/sol_1.py
@@ -16,8 +16,20 @@ def generate_prime_factors(num : int) -> list:
     '''
     Generates a list of prime factors of num and returns as a list
     '''
+    if num < 2: return []
+
     primes = [2,3,5,7,11,13,17,19]
-    pass
+    l = []
+    for i in primes:
+        if num == 1: break
+        c = 0
+        while num % i == 0:
+            num = num // i
+            c += 1
+        l.append( (i, c) )
+    
+    return l
+
 
 
 def solution() -> int:
@@ -25,5 +37,10 @@ def solution() -> int:
     Returns the smallest positive number that is evenly divisible by all of the numbers from 1 to 20
     '''
     pass
+
+if __name__ == "__main__":
+    print(generate_prime_factors(4))
+    print(generate_prime_factors(10))
+    print(generate_prime_factors(18))
 
 
