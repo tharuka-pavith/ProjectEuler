d0b45047274266a2775021ffdc418949ab0e7986
tharuka-pavith <tharukapavith2@gmail.com>
2022-02-14 21:38:43 +0530

    Update comments for solution()

--- problem_009/sol_1.py
+++ problem_009/sol_1.py
@@ -14,18 +14,36 @@ Solution: 31875000 Done!
 
 References: 
     - 
-
 '''
 from math import sqrt
 
-def solution(sum: int=1000) -> int:
+def solution() -> int:
     '''
-    Returns solution
-    TODO complete doc
+    Returns Pythagorean triplet for which a + b + c = 1000 where a^2 + b^2 = c^2
+    '''
+
+    '''
+    From c^2 = a^2 + b^2, a+b+c = 1000;
+
+    ab = 500(1000 - 2c)  ==>  c < 500
+
+    Let c = P and ab = R then,
+    b = R/a
+
+    => a^2 + b^2 = P^2
+    => a^2 + R^2/a^2 = P^2
+    => a^4 - P^2 * a^2 + R^2 = 0 (quadratic equation)
+
+    From x = -b +- sqrt(b^2 - 4ac)
+
+    First let k = aqrt(P^4 - 4R^2)
+    Then a^2 = (P^2 +- k) / 2
+
+    From above equation find, a, b
     '''
     for c in range(1, 500):
         P = c
-        R = ab = 500 * (1000 - 2 * c)
+        R =500 * (1000 - 2 * c) # R = ab
 
         k = sqrt(P**4 - 4*(R**2)) if P**4 - 4*(R**2) > 0 else 'invalid' # sqrt( b^2 - 4ac)
         if k == 'invalid': continue
